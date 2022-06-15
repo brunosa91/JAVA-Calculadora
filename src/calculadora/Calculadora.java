f5f7c3e99ec4a37a6ffd525651393fa312cc74e4
brunosa91 <brunosantana.engenharia@gmail.com>
2022-06-15 13:51:14 -0300

    Criado os métodos de soma e subtração

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -6,7 +6,14 @@ public class Calculadora {
 
         double resultado = num1 + num2;
 
-        System.out.println(num1 +"teste"+num2 +"" + resultado);
+        System.out.println("A soma dos números " + num1 +" e "+num2 +" tem o resultado de " + resultado);
+
+    }
+    public static void subtracao(double num1, double num2) {
+
+        double resultado = num1 - num2;
+
+        System.out.println("A subtração dos números " + num1 +" e "+num2 +" tem o resultado de " + resultado);
 
     }
 }
--- src/calculadora/Main.java
+++ src/calculadora/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("exr");
         Calculadora.soma(3, 6);
+        Calculadora.subtracao(7,4);
 
 
 
