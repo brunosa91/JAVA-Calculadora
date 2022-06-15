b83c0085bb90a550e28c317666d004d10602e73e
brunosa91 <brunosantana.engenharia@gmail.com>
2022-06-15 12:45:26 -0300

    Criado classe

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,12 @@
+package calculadora;
+
+public class Calculadora {
+
+    public static void soma(double num1, double num2) {
+
+        double resultado = num1 + num2;
+
+        System.out.println(num1 +""+num2 +"" + resultado);
+
+    }
+}
---
+++ src/calculadora/Main.java
@@ -0,0 +1,12 @@
+package calculadora;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("exr");
+        Calculadora.soma(3, 6);
+
+
+
+
+    }
+}
