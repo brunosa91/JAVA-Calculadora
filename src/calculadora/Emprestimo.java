f1c11d28cd84a234a330c0879ef89e3c698a18db
brunosa91 <brunosantana.engenharia@gmail.com>
2022-06-16 13:35:02 -0300

    Implementado o sistema de juros simples

--- src/calculadora/Emprestimo.java
+++ src/calculadora/Emprestimo.java
@@ -1,4 +1,20 @@
 package calculadora;
 
 public class Emprestimo {
+    
+    public static void emprestimo (int parcela, double valor){
+        
+        if (parcela <= 2){
+            System.out.println("O valor da parcela sem acŕecimo é " + valor/parcela);
+            
+        } else if (parcela > 2 && parcela <= 12) {
+
+
+            System.out.println("O valor da parcela com juros é " + (valor/parcela) * 1.02);
+            
+        }else{
+            System.out.println("Número de parcelas inválido");
+        }
+
+    }
 }
--- src/calculadora/Main.java
+++ src/calculadora/Main.java
@@ -5,6 +5,12 @@ public class Main {
         System.out.println("exr");
         Calculadora.soma(3, 6);
         Calculadora.subtracao(7,4);
+        Calculadora.multiplicacao(5,5);
+        Calculadora.divisao(12,4);
+
+        Emprestimo.emprestimo(5,1000);
+        Emprestimo.emprestimo(2,1000);
+        Emprestimo.emprestimo(13,1000);
 
 
 
