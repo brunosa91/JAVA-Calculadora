ac5b4abbabc39e897a9987604eceb1e45327b5ef
brunosa91 <brunosantana.engenharia@gmail.com>
2022-06-16 12:43:08 -0300

    criado o método de divisão

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -23,5 +23,12 @@ public class Calculadora {
         System.out.println("A multiplicação dos númetos " + num1 + " e " + num2 + " tem o resultado de " +resultado);
     }
 
+    public  static  void  divisao(double num1, double num2){
+
+        double resultado =  num1/num2;
+
+        System.out.println("A divisão dos númetos " + num1 + " e " + num2 + " tem o resultado de " +resultado);
+    }
+
 
 }
